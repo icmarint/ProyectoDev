aa9e43580f7f1092014f90c4789dc5de4c6eaa0f
Isabel Marín <icmarint@unal.edu.co>
2022-10-06 21:29:13 -0500

    variables JS y Html DOM

---
+++ 02-var-fn.js
@@ -0,0 +1,16 @@
+let fruta = 'manzana';
+
+function picar(fruta){
+    return fruta + ' picada';
+}
+
+let frutaPicada = picar(fruta)
+
+console.log(frutaPicada)
+
+let frutas = ['manzana', 'pera', 'auyama']
+
+let frutasPicadas = picar(frutas[1])
+
+console.log(frutasPicadas)
+
---
+++ index.js
@@ -0,0 +1,6 @@
+function resta(a,b,c){
+    return a - b - c;
+}
+    console.log(resta(5,2,1))
+
+    
